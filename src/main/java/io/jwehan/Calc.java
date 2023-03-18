30997fde4f300bbcff00d3fe3bb7e7cb94a8b2c3
본ìjwehan < jwehan@naver.com>
2023-03-18 15:59:22 +0900

    split는 문자의 양 공백을 지워준다! 좌공백 우공백 따지며 코드를 작성해줘야 한다.. /*테마를 바꿧더니 length 문이 빨갛게 보여서 에러인줄 알았다*/

--- src/main/java/io/jwehan/Calc.java
+++ src/main/java/io/jwehan/Calc.java
@@ -3,16 +3,16 @@ package io.jwehan;
 public class Calc {
     public static int run(String exp){
 
-        exp = exp.replaceAll("-", "+ -");
+        exp = exp.replaceAll(" - ", "+ -");
 
-        String[] split = exp.split("\\+");
+        String[] ttd = exp.split("\\+");
 
-        int a = Integer.parseInt(split[0].trim());
-        int b = Integer.parseInt(split[1].trim());
+        int a = Integer.parseInt(ttd[0].trim());
+        int b = Integer.parseInt(ttd[1].trim());
         int c = 0;
 
-        if ( split.length > 2){
-            c = Integer.parseInt(split[2]);
+        if ( ttd.length > 2){
+            c = Integer.parseInt(ttd[2]);
         }
         return a+ b+ c;
     }
