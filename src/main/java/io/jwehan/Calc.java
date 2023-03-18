650aee340f0b7d68726867f6a53ae63bdbb3e039
본ìjwehan < jwehan@naver.com>
2023-03-18 12:51:04 +0900

    gradle dependencies 문제 import 목록에 안나오는건 버전 문제인가

--- src/main/java/io/jwehan/Calc.java
+++ src/main/java/io/jwehan/Calc.java
@@ -1,4 +1,8 @@
 package io.jwehan;
 
 public class Calc {
+    public static int run(String exp){
+
+        return 2;
+    }
 }
--- src/test/java/io/jwehan/CalcTest.java
+++ src/test/java/io/jwehan/CalcTest.java
@@ -1,7 +1,17 @@
 package io.jwehan;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class CalcTest {
+
+    @Test
+    @DisplayName("1 + 1 == 2")
+    void t1(){
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+
+    }
 
 }
